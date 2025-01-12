29f47041eca4c9135f1088d3ac6d2728d8956165
Sarunas Jaraminas <jaraminas.sarunas@gmail.com>
2025-01-12 21:27:23 +0200

    Updated with langchain

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Chat from '@/components/Chat';
-
-export default function Home() {
-  return (
-    <main className="flex justify-center items-center h-screen">
-      <Chat />
-    </main>
-  );
-}
